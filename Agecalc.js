f25727d265de16e952113d337069522dcb0690b3
Sai Swaroop <gujaratisaiswaroop@gmail.com>
2017-12-20 09:27:16 +0530

    Files added

---
+++ Agecalc.js
@@ -0,0 +1,4 @@
+var fullname = prompt("Enter your name!");
+var age=prompt("Enter your current age!")
+var days = age * 365.25; // .25 includes the leap years account
+alert(fullname +" "+"your "+ age + " years is roughly "+ days + " days ");
